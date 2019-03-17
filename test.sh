bdd66e4de5c45010909ec43263af21ed67b0247f
Ninja Dynamics <brunosilva.work@gmail.com>
2019-03-17 14:58:13 +0100

    Improve test script

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-cat pydata.cpp functest.cpp > pydata_test.cpp && g++ pydata_test.cpp -o pydata && valgrind --leak-check=full --track-origins=yes ./pydata
-rm pydata_test.cpp
+cmd="$1 -std=c++$2"
+cat pydata.cpp functest.cpp > pydata_test.cpp && $cmd -Wall pydata_test.cpp -o pydata && valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all ./pydata && rm pydata_test.cpp
+echo "Compiled using: $cmd"
